d0ff516c82866c406d41bca667778fb245acb199
jisucoder <jisulove735@gmail.com>
2022-11-15 11:52:43 +0900

    렌더링

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/no-reserved-component-names -->
+<template>
+  <Header />
+  <RouterView />
+</template>
+
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~/scss/main";
+</style>
