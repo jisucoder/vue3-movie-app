06877f27f023b3fed4b8b3411d5ddad6df9dfa9b
jisucoder <jisulove735@gmail.com>
2022-11-22 15:09:41 +0900

    continue

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'JiSu',
+    email: 'jisulove735@gmail.com',
+    blog: '',
+    phone: '+82-10-1234-5678',
+    image: 'https://www.water.or.kr/images/egovframework/life/weast/weast044_01.jpg'
+  })
+}
